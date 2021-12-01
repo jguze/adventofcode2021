cdaa4a0d8a024e2f89567b73141de8bb38fbd2ed
Justin Guze <jaguze@gmail.com>
2021-12-01 11:23:14 -0800

    Day 1 solutions

---
+++ day1/src/main.rs
@@ -0,0 +1,73 @@
+use std::cmp;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
+    let file = File::open("input1.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut increased = 0;
+    let mut decreased = 0;
+    let mut prev_num: Option<u32> = None;
+
+    for line in reader.lines() {
+        let num = line
+            .expect("Read line correctly")
+            .to_string()
+            .parse::<u32>()
+            .unwrap();
+        if prev_num.is_some() {
+            if prev_num.unwrap() < num {
+                increased += 1;
+            } else if prev_num.unwrap() > num {
+                decreased += 1;
+            }
+        }
+        prev_num = Some(num);
+    }
+
+    println!(
+        "Part 1\nIncreased - {}\nDecreased - {}",
+        increased, decreased
+    );
+}
+
+fn part2() {
+    let file = File::open("input2.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut increased = 0;
+    let mut nums = vec![];
+    let window = 3;
+
+    for line in reader.lines() {
+        let num = line
+            .expect("Read line correctly")
+            .to_string()
+            .parse::<u32>()
+            .unwrap();
+        nums.push(num);
+    }
+
+    let mut prev_num: Option<u32> = None;
+    let max = nums.len();
+    for start in 0..max {
+        let end = cmp::min(start + window, max);
+        let current_num: u32 = nums[start..end].iter().sum();
+
+        if prev_num.is_some() {
+            if prev_num.unwrap() < current_num {
+                increased += 1;
+            }
+        }
+
+        prev_num = Some(current_num);
+    }
+
+    println!("Part2\nIncreased - {}", increased);
+}
